e1417beaa4cdae5e953418b9bb3b578b3a2fff9c
Siddharth M <156581620+Anduiln@users.noreply.github.com>
2024-03-29 12:05:11 +0000

    Make button to generate new grid
    Single length value to maintain square grid.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("#reset");
 
 createGrid(container, 16, 16);
-
 const squareList = document.querySelectorAll(".square");
-
 squareList.forEach((item) => item.addEventListener("mouseover", changeColor));
 
+button.addEventListener("click", generateUserGrid);
 
 function createSquare (parent) {
     const square = document.createElement("div");
@@ -31,4 +31,21 @@ function createGrid (parent, height, width) {
 
 function changeColor (event) {
     event.target.className = "square hover";
+}
+
+function generateUserGrid () {
+    let length = prompt("How many tiles per side of the square grid?", 16);
+    if (length > 100) {
+        alert("Please enter a value under 100.");
+        return;
+    }
+    
+    const lineList = document.querySelectorAll(".line");
+    for (line of lineList) {
+        container.removeChild(line);
+    }
+
+    createGrid(container, length, length);
+    const squareList = document.querySelectorAll(".square");
+    squareList.forEach((item) => item.addEventListener("mouseover", changeColor));
 }
