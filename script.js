c05dc1525ba981c4bb0676ed4efbe5e12ca0cdb8
Siddharth M <156581620+Anduiln@users.noreply.github.com>
2024-03-29 11:27:15 +0000

    Create function to change color on hover
     Via CSS class change for easier testing.

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const container = document.querySelector("#container");
 
 createGrid(container, 16, 16);
 
+const squareList = document.querySelectorAll(".square");
+
+squareList.forEach((item) => item.addEventListener("mouseover", changeColor));
+
 
 function createSquare (parent) {
     const square = document.createElement("div");
@@ -23,4 +27,8 @@ function createGrid (parent, height, width) {
     for (let i = 1 ; i <= height ; i++) {
         createLine(parent, width);
     }
+}
+
+function changeColor (event) {
+    event.target.className = "square hover";
 }
